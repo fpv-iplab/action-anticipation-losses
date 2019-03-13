6345251b036399bcb4de3d08d69bddd4c4d8f40c
Evangelos Kazakos <kazakosv90@gmail.com>
2019-03-13 18:06:24 +0000

    Support for PyTorch 0.4.1 (#1)
    * PyTorch 0.4.1 support
    
    * PyTorch 0.4.1 support

--- MarginalCrossEntropyLoss/MarginalCrossEntropyLoss.py
+++ MarginalCrossEntropyLoss/MarginalCrossEntropyLoss.py
@@ -2,8 +2,9 @@ from torch import nn
 import torch
 from torch.autograd import Variable
 import numpy as np
-from torch.nn.modules.loss import _Loss, _assert_no_grad
+from torch.nn.modules.loss import _Loss
 from torch.nn import functional as F
+
 
 class MarginalCrossEntropyLoss(_Loss):
     """Implements the "Marginal Cross Entropy Loss" from the paper:
@@ -11,7 +12,7 @@ class MarginalCrossEntropyLoss(_Loss):
         Leveraging Uncertainty to Rethink Loss Functions and Evaluation Measures for Egocentric Action Anticipation . 
         In International Workshop on Egocentric Perception, Interaction and Computing (EPIC) in conjunction with ECCV ."
     """
-    def __init__(self, marginal_indexes, numclass, size_average=True, reduce=True):
+    def __init__(self, marginal_indexes, numclass, reduction='mean'):
         """Marginal Cross Entropy Loss
         Input:
             marginal_indexes: list of indexes for each of the marginal probabilities
@@ -19,13 +20,12 @@ class MarginalCrossEntropyLoss(_Loss):
             size_average: whether to average the losses in the batch (if False, they are summed)
             reduce: if False, all individual losses are applied, otherwise they are summed/averaged depending on size_average
             """
-        super(MarginalCrossEntropyLoss, self).__init__(size_average=size_average)
-        self.reduce=reduce
+        super().__init__(reduction=reduction)
         marginal_masks = []
         for mi in marginal_indexes:
-            masks=[]
+            masks = []
             for i in mi:
-                masks.append(self.__indexes_to_masks(i,numclass))
+                masks.append(self.__indexes_to_masks(i, numclass))
             marginal_masks.append(torch.stack(masks))
 
         self.marginal_masks = marginal_masks
@@ -33,7 +33,7 @@ class MarginalCrossEntropyLoss(_Loss):
     def __indexes_to_masks(self, indexes, maxlen):
         mask = np.zeros(maxlen)
         for i in indexes:
-            mask[i]=1
+            mask[i] = 1
         return torch.from_numpy(mask)
 
     def __sum_exps(self, exps):
@@ -41,29 +41,29 @@ class MarginalCrossEntropyLoss(_Loss):
 
     def __build_marginal_loss(self, input, marginal_target, marginal_masks):
         mask = torch.Tensor(marginal_masks[marginal_target.cpu().data].float())
-        mask=mask.to(input.device)
-        exps=torch.exp(input)
-        sum_all=self.__sum_exps(exps)
-        sum_marginal=exps.mul(mask).sum(1)
-        return torch.log(sum_all)-torch.log(sum_marginal)
-        
+        mask = mask.to(input.device)
+        exps = torch.exp(input)
+        sum_all = self.__sum_exps(exps)
+        sum_marginal = exps.mul(mask).sum(1)
+        return torch.log(sum_all) - torch.log(sum_marginal)
+
     def forward(self, input, marginal_targets):
         """  input: predicted scores
              marginal_targets: list of targets of the marginal probabilities"""
         #input: bs x nc
         #marginal_targets: list of marginal targets
         loss = torch.Tensor(torch.zeros(input.shape[0])).to(input.device)
-        for i,mm in enumerate(self.marginal_masks):
-            l=self.__build_marginal_loss(input,marginal_targets[:,i],mm)
-            loss+=l
+        for i, mm in enumerate(self.marginal_masks):
+            l = self.__build_marginal_loss(input, marginal_targets[:, i], mm)
+            loss += l
 
         #sum cross entropy on actions
-        loss+=F.cross_entropy(input,marginal_targets[:,-1], size_average=False, reduce=False)
+        loss += F.cross_entropy(input, marginal_targets[:, -1], reduction='none')
 
-        if self.reduce:
-            if self.size_average:
-                loss=loss.mean()
-            else:
-                loss=loss.sum()
-        
+        if self.reduction is not None:
+            if self.reduction == 'mean':
+                loss = loss.mean()
+            elif self.reduction == 'sum':
+                loss = loss.sum()
+
         return loss
--- TruncatedTopkEntropyLoss/TruncatedTopkEntropyLoss.py
+++ TruncatedTopkEntropyLoss/TruncatedTopkEntropyLoss.py
@@ -2,8 +2,9 @@ from torch import nn
 import torch
 from torch.autograd import Variable
 import numpy as np
-from torch.nn.modules.loss import _Loss, _assert_no_grad
+from torch.nn.modules.loss import _Loss
 from torch.nn import functional as F
+
 
 class TruncatedTopkEntropyLoss(_Loss):
     """Implements the Truncated TOP-K Entropy Loss from the paper
@@ -12,30 +13,28 @@ class TruncatedTopkEntropyLoss(_Loss):
         "Loss functions for top-k error: Analysis and insights." 
         Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition. 2016.
     """
-    def __init__(self, k, size_average=True, reduce=True):
-        super(TruncatedTopkEntropyLoss, self).__init__(size_average=size_average)
-        assert(k>1) #for k=1 use standard cross entropy
+
+    def __init__(self, k, reduction='mean'):
+        super().__init__(reduction=reduction)
+        assert(k > 1)  # for k=1 use standard cross entropy
         self.k = k
-        self.reduce=reduce
 
     def forward(self, input, target):
-        _assert_no_grad(target)
-        fy = input[range(target.size()[0]),target]
+        fy = input[range(target.size()[0]), target]
         fj = input
 
-        aj = fj - fy.view(-1,1)
+        aj = fj - fy.view(-1, 1)
 
-        if self.k!=1:
-            aj[range(target.size()[0]),target]=np.inf #set positions where the target is to infinity to avoid select them among the smalles m-k components
-            aj=aj.topk(aj.shape[1]-self.k,largest=False)[0] #select the smallest m-k components
-        
-        loss = torch.log(1+torch.exp(aj).sum(1))
+        if self.k != 1:
+            aj[range(target.size()[0]), target] = np.inf  # set positions where the target is to infinity to avoid select them among the smalles m-k components
+            aj = aj.topk(aj.shape[1] - self.k, largest=False)[0]  # select the smallest m-k components
 
-        if self.reduce:
-            if self.size_average:
-                loss=loss.mean()
-            else:
-                loss=loss.sum()
+        loss = torch.log(1 + torch.exp(aj).sum(1))
+
+        if self.reduction is not None:
+            if self.reduction == 'mean':
+                loss = loss.mean()
+            elif self.reduction == 'sum':
+                loss = loss.sum()
 
         return loss
-
